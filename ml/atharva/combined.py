081b7553cb13efb2445ad1937e239ae29db520db
atharva-diwan <diwanatharva@gmail.com>
2020-08-22 16:20:15 +0530

    added file for safe keys

--- ml/atharva/combined.py
+++ ml/atharva/combined.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import cv2
-from skimage.feature import hog
+
 import numpy as np
-import time
-import cv2
-import os
-import darknet
 # from sklearn.externals import joblib
-import joblib
+from PIL import Image, ImageOps
+import PIL.Image
+from skimage import transform
 # import tensorflow.keras as keras
 from tensorflow.keras.models import load_model
 #function for asserting color configuratoin
@@ -46,20 +44,22 @@ def build_model():
     return model
 
 model = build_model()
-model.load_weights('/home/vishwajeet/catkin_ws/src/ethan_control/test.h5')
+model.load_weights('/home/atharva/catkin_ws/src/turtlebot3/video_generator/scripts/test.h5')
+def load(im):
 
+   nk=np.zeros((1,28,28,1),dtype='float')
+   np_image = np.array(im).astype('float32')/255
+   nk[0,:,:,0]=np_image
+#np_image = transform.resize(np_image, (28, 28, 1))
+   #np_image = np.expand_dims(np_image, axis=0)
+   return nk
 def predictor(model_name,img):
   global model
-  nl=np.array(img,dtype='float')
-  nk=np.zeros((1,28,28,1),dtype='float')
-  nk[0,:,:,0]=nl
-  res=model.predict_on_batch(nk)
-  res=np.around(res)
-  for i in range(res.shape[1]):
-    if res[0,i]==1:
-
-      print(i)
-      return i
+  image = load(img)
+  res=model.predict(image)
+  i=np.argmax(res, axis=1)
+  prob=res[0,i]
+  return i,prob
 def assert_color(img):
     ar=np.array(img,dtype='int')
     cnt=0
@@ -72,97 +72,15 @@ def assert_color(img):
                 else:
                     continue
     return 0
-"""
-labelsPath = os.path.join("obj.names")
-LABELS = open(labelsPath).read().strip().split("\n")
-
-# derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.join("yolov3_custom_train_500.weights")
-configPath = os.path.join("yolov3_custom_train.cfg")
-# Loading the neural network framework Darknet (YOLO was created based on this framework)
-net = cv2.dnn.readNetFromDarknet(configPath,weightsPath)
-def predict(image):
-    
-    # initialize a list of colors to represent each possible class label
-    np.random.seed(42)
-    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
-    (H, W) = image.shape[:2]
-    
-    # determine only the "ouput" layers name which we need from YOLO
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-    
-    # construct a blob from the input image and then perform a forward pass of the YOLO object detector, 
-    # giving us our bounding boxes and associated probabilities
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    layerOutputs = net.forward(ln)
-    
-    boxes = []
-    confidences = []
-    classIDs = []
-    threshold = 0.2
-    
-    # loop over each of the layer outputs
-    for output in layerOutputs:
-        # loop over each of the detections
-        for detection in output:
-            # extract the class ID and confidence (i.e., probability) of
-            # the current object detection
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-
-            # filter out weak predictions by ensuring the detected
-            # probability is greater than the minimum probability
-            # confidence type=float, default=0.5
-            if confidence > threshold:
-                # scale the bounding box coordinates back relative to the
-                # size of the image, keeping in mind that YOLO actually
-                # returns the center (x, y)-coordinates of the bounding
-                # box followed by the boxes' width and height
-                box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype("int")
-
-                # use the center (x, y)-coordinates to derive the top and
-                # and left corner of the bounding box
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
-
-                # update our list of bounding box coordinates, confidences,
-                # and class IDs
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                classIDs.append(classID)
-
-    # apply non-maxima suppression to suppress weak, overlapping bounding boxes
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, threshold, 0.1)
-
-    # ensure at least one detection exists
-    if len(idxs) > 0:
-        # loop over the indexes we are keeping
-        for i in idxs.flatten():
-            # extract the bounding box coordinates
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
-
-            # draw a bounding box rectangle and label on the image
-            color = (255,0,0)
-            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            text = "{}".format(LABELS[classIDs[i]], confidences[i])
-            cv2.putText(image, text, (x +15, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                1, color, 2)
-    return image
-    """
+      
 def image_callback(img_msg):
 
-	boundaries={{'blue':([110,50,50], [130,255,255]),
-            'orange':([15,50,50], [20,255,255]),
-            'red':([0,150,50], [10,255,255]),
-            'yellow':([25,150,50], [35,255,255]),
-            'green':([45,150,50], [65,255,255])   
+	boundaries={'blue':([110,150,150], [130,255,255]),
+            'orange':([15,150,150], [20,255,255]),
+            'red':([0,150,150], [7,255,255]),
+            'yellow':([25,150,150], [35,255,255]),
+            'green':([55,150,150], [65,255,255])   
            }   
-    	        }
 
 	
 	#read the converted input image
@@ -205,7 +123,7 @@ def image_callback(img_msg):
 			mask = cv2.inRange(hsv, lower, upper)
 			# find the colors within the specified boundaries and apply
             # the mask
-			if assert_color(mask)== 1:     #alternative: if cv2.countNonZero(mask) != 0:
+			if cv2.countNonZero(mask) != 0:     #alternative: if cv2.countNonZero(mask) != 0:
 				color=key
 				break
 		# Make the rectangular region around the digit
@@ -220,8 +138,10 @@ def image_callback(img_msg):
 		except:
 			continue
         # Calculate the HOG features
-		cv2.putText(im, str(predictor('my_model.h5',roi))+' '+str(color), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX,1, (0, 255, 255), 2)
-        im=detect(net,meta,im)
+		cls , prob = predictor('my_model.h5',roi)
+		if prob>0.7:
+				
+			cv2.putText(im, str(cls)+' '+str(color), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX,1, (0, 255, 255), 2)
 	cv2.imshow('img',im)
     #show  the frame with detection
 	cv2.waitKey(3)		
@@ -232,7 +152,7 @@ if __name__ == '__main__':
 		#initialise the ros node
 		rospy.init_node('realtime_test', anonymous=True)
 		# Initalize a subscriber to the "/camera/rgb/image_raw" topic with the function "image_callback" as a callback
-		sub_image = rospy.Subscriber("/camera1/image_raw", Image, image_callback)
+		sub_image = rospy.Subscriber("/camera/rgb/image_raw", Image, image_callback)
 		# Initialize the CvBridge class
 		bridge=CvBridge()
 		rospy.spin()
